690581849a75dac6d6aba70d9d4be68379d83b0c
Paul Rosania <paul.rosania@gmail.com>
2012-08-01 22:34:28 -0700

    Move public and views directories to the root

--- lib/rigor/web/server.rb
+++ lib/rigor/web/server.rb
@@ -3,12 +3,9 @@ require 'rigor'
 
 module Rigor::Web
   class Server < Sinatra::Base
-    set :root, File.dirname(File.expand_path(__FILE__))
+    set :root, File.join(File.dirname(File.expand_path(__FILE__)), '..', '..', '..')
 
     set :static, true
-
-    set :public_folder, Proc.new { File.join(root, "server", "public") }
-    set :views,         Proc.new { File.join(root, "server", "views")  }
 
     helpers do
       include Rack::Utils
